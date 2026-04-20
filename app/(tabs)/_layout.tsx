09aaaee2e917a564429424b41eea02a4b6b83dcf
Alori Daniel <aloridaniel123@gmail.com>
2026-04-20 12:19:15 +0100

    chore: icon change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import { fonts } from "@/constants/fonts";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Platform } from "react-native";
 import { KeyboardController } from "react-native-keyboard-controller";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -17,10 +18,10 @@ export default function TabLayout() {
         tabBarActiveTintColor: "#0D3B66",
         tabBarInactiveTintColor: "#7E8B9A",
         tabBarStyle: {
-          height: 84,
+          height: 84 + (Platform.OS === "android" ? insets.bottom : 0),
           paddingTop: 8,
           paddingHorizontal: 8,
-          paddingBottom: insets.bottom + 15,
+          paddingBottom: 15,
           backgroundColor: "#FFFDF8",
           borderTopColor: "#E9E0D2",
         },
